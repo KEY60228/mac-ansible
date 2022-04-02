b7afab0ea423fc230d6a26416d0d3f0eb900c98a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-02 12:53:01 +0900

    M1用に分岐の追加

--- init.sh
+++ init.sh
@@ -2,9 +2,18 @@
 
 ANSIBLE_PATH=~/ansible
 
-## command line toolsとhomebrew、ansibleのインストール
+## command line toolsとhomebrewのinstall
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## architecture check
+if [ `uname -m` = "arm64" ]; then
+    ## M1の場合homebrewのパスを通す
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    source ~/.zprofile
+fi
+
+## ansibleのinstall
 brew install ansible
 
 ## repositoryのclone
